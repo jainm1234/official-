48afba67092a681be337219efc260e47e8408f4c
malvika jain <malvika.jain@icloud.com>
2016-05-09 14:33:07 -0700

    set up

---
+++ Cipher/src/Receive.java
@@ -0,0 +1,9 @@
+
+public class Receive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Cipher/src/RijndaelMethods.java
@@ -0,0 +1,11 @@
+
+public interface RijndaelMethods {
+	byte[][] RoundKey(byte[][] words);
+
+	byte[][] SubBytes(byte[][] words);
+
+	byte[][] ShiftRows(byte[][] words);
+
+	byte[][] MixColumns(byte[][] words);
+
+}
